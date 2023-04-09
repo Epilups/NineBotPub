f8448daea39ae8a23f9643914e854c9e52be4683
Epilups <emils10.pilups@gmail.com>
2023-04-09 18:15:56 +0300

    yes

--- CoreBot/Program.cs
+++ CoreBot/Program.cs
@@ -4,8 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-        Environment.SetEnvironmentVariable("ASPNETCORE_URLS", $"https://*:{port}");
         var bot = new Bot();
         bot.RunAsync().GetAwaiter().GetResult();
     }
