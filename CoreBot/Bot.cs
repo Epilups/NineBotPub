72361c3b667c8317f058d8f7b30547bd6b0fac32
Epilups <emils10.pilups@gmail.com>
2023-04-11 11:59:22 +0300

    token

--- CoreBot/Bot.cs
+++ CoreBot/Bot.cs
@@ -23,7 +23,8 @@ public class Bot
         var json = string.Empty;
         var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
         Console.WriteLine(projectFolder);
-        var file = Path.Combine(projectFolder, "CoreBot/configuration.json");
+        var file = Path.Combine(projectFolder, "CoreBot\\configuration.json");
+        Console.WriteLine(file);
         await using (var fs = File.OpenRead(file))
         using (var sr = new StreamReader(fs, new UTF8Encoding(false))) json = await sr.ReadToEndAsync();
 
