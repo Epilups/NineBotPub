17e0e7af49a1680694f40a65e1489dce3779a544
Epilups <emils10.pilups@gmail.com>
2023-04-11 12:14:18 +0300

    token

--- CoreBot/Bot.cs
+++ CoreBot/Bot.cs
@@ -21,24 +21,12 @@ public class Bot
     {
         
         var json = string.Empty;
-        var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\"));
+        var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"../../../../"));
         Console.WriteLine(projectFolder);
         
         DirectoryInfo place = new DirectoryInfo(projectFolder);
-     
-        // Using GetFiles() method to get list of all
-        // the files present in the Train directory
-        FileInfo[] Files = place.GetFiles();
-        Console.WriteLine("Files are:");
-        Console.WriteLine();
-     
-        // Display the file names
-        foreach(FileInfo i in Files)
-        {
-            Console.WriteLine("File Name - {0}", i.Name);
-        }
-        
-        var file = Path.Combine(projectFolder, "CoreBot\\configuration.json");
+
+        var file = Path.Combine(projectFolder, @"CoreBot/configuration.json");
         Console.WriteLine(file);
         await using (var fs = File.OpenRead(file))
         using (var sr = new StreamReader(fs, new UTF8Encoding(false))) json = await sr.ReadToEndAsync();
