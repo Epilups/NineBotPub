6986a57f1656980147ac96d17016655763dfaae2
Epilups <emils10.pilups@gmail.com>
2023-07-18 17:21:07 +0300

    Testing buttons

--- CoreBot/Bot.cs
+++ CoreBot/Bot.cs
@@ -1,8 +1,10 @@
-﻿using CoreBot.Slash_Commands.Game_Commands;
+﻿using CoreBot.Slash_Commands;
+using CoreBot.Slash_Commands.Game_Commands;
 using CoreBot.Slash_Commands.Random_commands;
 using CoreBot.Slash_Commands.Utilities;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
@@ -33,6 +35,8 @@ public class Bot
         });
         
         Client.Ready += OnClientReady;
+
+        Client.ComponentInteractionCreated += ButtonPressResponse;
         
         var commandsConfig = new CommandsNextConfiguration()
         {
@@ -59,12 +63,27 @@ public class Bot
         slashCommandsConfig.RegisterCommands<PolarBear>();
         slashCommandsConfig.RegisterCommands<Trivia>();
         slashCommandsConfig.RegisterCommands<BlackTeas>();
+        slashCommandsConfig.RegisterCommands<TestingComponents>();
         
         
         await Client.ConnectAsync();
         await Task.Delay(-1);
     }
-    
+
+    private async Task ButtonPressResponse(DiscordClient sender, ComponentInteractionCreateEventArgs e)
+    {
+        if (e.Interaction.Data.CustomId == "1")
+        {
+            await e.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage,
+                new DiscordInteractionResponseBuilder().WithContent("Button press 1"));
+        }
+        else if (e.Interaction.Data.CustomId == "2")
+        {
+            await e.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage,
+                new DiscordInteractionResponseBuilder().WithContent("Button press 2"));
+        }
+    }
+
     private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
     {
         return Task.CompletedTask;
